a6989840967962318b99c05bf2d233d7dc7c8d43
Nataliia Koval <hakunata@gmail.com>
2021-02-01 00:09:58 +0200

    Quiz, ActiveQuiz components

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ActiveQuiz.css';
+
+const ActiveQuiz = props => (
+    <div className='ActiveQuiz'>
+        <p className='Question'>
+            <span>
+                <strong>2.</strong>&nbsp;
+                How are you?
+            </span>
+
+            <small>4 из 12</small>
+        </p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+)
+
+export default ActiveQuiz;
